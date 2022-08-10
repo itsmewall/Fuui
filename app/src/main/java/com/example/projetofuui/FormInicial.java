ce2365d45ef41694b1f68a8beaf393ed533428b3
Lucas Ribeiro <lucasribeiro37@gmail.com>
2022-08-10 20:40:54 -0300

    Correção

--- app/src/main/java/com/example/projetofuui/FormInicial.java
+++ app/src/main/java/com/example/projetofuui/FormInicial.java
@@ -1,13 +1,14 @@
 package com.example.projetofuui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class FormCadastro extends AppCompatActivity {
+public class FormInicial extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_cadastro);
+        setContentView(R.layout.activity_form_inicial);
     }
 }
