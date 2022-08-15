1e163f027c9f4ea42af65c57a2279f4164478271
Lucas Ribeiro <lucasribeiro37@gmail.com>
2022-08-14 23:46:04 -0300

    Barra de Navegação

--- app/src/main/java/com/example/projetofuui/FormInicial.java
+++ app/src/main/java/com/example/projetofuui/FormInicial.java
@@ -1,14 +1,35 @@
 package com.example.projetofuui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.projetofuui.databinding.ActivityFormInicialBinding;
 
 public class FormInicial extends AppCompatActivity {
+
+    private ActivityFormInicialBinding binding;
+    
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form_inicial);
+        binding = ActivityFormInicialBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initNavigation();
+                
     }
+    
+    private void initNavigation(){
+        navHostFragment 
+                = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+    }
+    
 }
