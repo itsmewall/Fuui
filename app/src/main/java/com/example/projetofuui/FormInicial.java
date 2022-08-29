6fd5dc2ff7f16d8aaa15e5c4386dd07fb9d60bae
Lucas Ribeiro <lucasribeiro37@gmail.com>
2022-08-29 10:07:51 -0300

    Correção dos nomes

--- app/src/main/java/com/example/projetofuui/FormInicial.java
+++ app/src/main/java/com/example/projetofuui/FormInicial.java
@@ -2,7 +2,6 @@ package com.example.projetofuui;
 
 import android.os.Bundle;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
