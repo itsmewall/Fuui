c76d944992fa24364aacbe324cf9b4e64caab4a3
Wallace Oliveira <89879348+itsmewall@users.noreply.github.com>
2022-08-30 13:38:29 -0300

    register & login workable

--- app/src/main/java/com/example/projetofuui/view/formLogin/FormLogin.java
+++ app/src/main/java/com/example/projetofuui/view/formLogin/FormLogin.java
@@ -2,29 +2,36 @@ package com.example.projetofuui.view.formLogin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.projetofuui.MainActivity;
 import com.example.projetofuui.MapsActivity;
 import com.example.projetofuui.R;
+import com.example.projetofuui.view.formCadastro.FormCadastro;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class FormLogin extends AppCompatActivity {
 
     private TextView hi;
+    private TextView text_cadastro2;
     private EditText email;
     private EditText password;
     private Button Button;
     private FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener firebaseAuthListener;
 
-    public FormLogin() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +51,50 @@ public class FormLogin extends AppCompatActivity {
 
         };
 
-        Button.setOnClickListener(view -> {
-            String registerEmail = email.getText().toString();
-            String senha = password.getText().toString();
+        Button button = (Button) findViewById(R.id.button);
+        Button text_cadastro2 = (Button) findViewById(R.id.text_cadastro2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String login = email.getText().toString();
+                String senha = password.getText().toString();
 
-            mAuth.signInWithEmailAndPassword(registerEmail, senha).addOnCompleteListener(FormLogin.this, task -> {
-                if (!task.isSuccessful()) {
-                    Toast.makeText(FormLogin.this, "Erro no login", Toast.LENGTH_SHORT).show();
+                if(!TextUtils.isEmpty(login) || !TextUtils.isEmpty(senha)) {
+                    mAuth.signInWithEmailAndPassword(login,senha)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        abrirTelaPrincipal();
+                                    } else {
+                                        String error =task.getException().getMessage();
+                                        Toast.makeText(FormLogin.this, "", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
                 }
+
+            }
         });
-    });
-}}
+        text_cadastro2.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  abrirCadastro();
+
+              }
+          });
+}
+
+
+    private void abrirTelaPrincipal(){
+        Intent intent = new Intent(FormLogin.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void abrirCadastro(){
+        Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+        startActivity(intent);
+        finish();
+    }
+}
